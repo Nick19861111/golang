b591157a76f775754df8c07b456b944fd7346ca4
nick <huada9559@gmail.com>
2024-06-18 16:00:18 +0800

    添加了监控的相关操作

--- user/main.go
+++ user/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"common/config"
+	"common/metrics"
 	"flag"
 	"fmt"
 )
@@ -14,7 +15,16 @@ func main() {
 	config.InitConfig(*configFile)
 	fmt.Println(config.Conf)
 	//2.启动监控
-
+	//启动携程
+	go func() {
+		err := metrics.Serve(fmt.Sprintf("0.0.0.0:%d", config.Conf.MetricPort))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("携程已经启动")
+	}()
+	for {
+	}
 	//3.启动程序 grpc的服务
 
 }
