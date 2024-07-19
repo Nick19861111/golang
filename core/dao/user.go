8e729b6c0768f8a4fbc9893e12a81508e0ac0a77
Nick19861111 <114399678+Nick19861111@users.noreply.github.com>
2024-07-19 13:15:49 +0800

    在userdao添加了获得地址的方法

--- core/dao/user.go
+++ core/dao/user.go
@@ -12,6 +12,7 @@ type UserDao struct {
 	repo *repo.Manager
 }
 
+// 通过id查询用户是否存在
 func (d *UserDao) FindUserByUid(ctx context.Context, uid string) (*entity.User, error) {
 	db := d.repo.Mongo.Db.Collection("user")
 	singleResult := db.FindOne(ctx, bson.D{
@@ -28,14 +29,30 @@ func (d *UserDao) FindUserByUid(ctx context.Context, uid string) (*entity.User, 
 	return user, nil
 }
 
+// 插入用户到数据库
 func (d *UserDao) Insert(ctx context.Context, user *entity.User) error {
 	db := d.repo.Mongo.Db.Collection("user")
 	_, err := db.InsertOne(ctx, user)
 	return err
 }
 
+// 创建用户的dao对象
 func NewUserDao(m *repo.Manager) *UserDao {
 	return &UserDao{
 		repo: m,
 	}
+}
+
+// 根据用户的id更新地址
+func (d *UserDao) UpdateUserAddressByUid(ctx context.Context, user *entity.User) error {
+	db := d.repo.Mongo.Db.Collection("user")
+	_, err := db.UpdateOne(ctx, bson.M{
+		"uid": user.Uid,
+	}, bson.M{
+		"$set": bson.M{
+			"address":  user.Address,
+			"location": user.Location,
+		},
+	})
+	return err
 }
